8ba8d6d907c85be83a3bd69c26be9163247b3cba
sowmiya99 <sowmi.chriss@gmail.com>
2015-03-19 17:24:21 +0530

    Update HelloActivity.java

--- src/main/java/org/hello/HelloActivity.java
+++ src/main/java/org/hello/HelloActivity.java
@@ -19,7 +19,7 @@ public class HelloActivity extends Activity {
         super.onStart();
         LocalTime currentTime = new LocalTime();
         TextView textView = (TextView) findViewById(R.id.text_view);
-		textView.setText(" Let me try ! :)Yuuuupsedfafe!Wellllcome to Android Appppplicatttion !!! ");
+		textView.setText(" Let me try ! :)Yuuuupdfafe!Wellllcome to Android Appppplicatttion !!! ");
         textView.setText("The current local timmme isss: " + currentTime);
     }
 
